c4d53ff7f4a2c2be920465d5be763d1db194e7b6
Neil McBride <Neil.McBride@digital.homeoffice.gov.uk>
2019-01-10 09:28:10 +0000

    initial commit

---
+++ main.tf
@@ -0,0 +1,44 @@
+locals {
+  naming_suffix = "fms-${var.naming_suffix}"
+}
+
+resource "aws_subnet" "fms" {
+  vpc_id                  = "${var.appsvpc_id}"
+  cidr_block              = "${var.fms_cidr_block}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${var.az}"
+
+  tags {
+    Name = "subnet-${local.naming_suffix}"
+  }
+}
+
+resource "aws_security_group" "fms" {
+  vpc_id = "${var.appsvpc_id}"
+
+  tags {
+    Name = "sg-fms-${local.naming_suffix}"
+  }
+
+  ingress {
+    from_port = 5432 
+    to_port   = 5432 
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "${var.data_pipe_apps_cidr_block}",
+      "${var.opssubnet_cidr_block}",
+      "${var.peering_cidr_block}"
+    ]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "appsvpc_id" {}
+variable "opssubnet_cidr_block" {}
+variable "data_pipe_apps_cidr_block" {}
+variable "fms_cidr_block" {}
+variable "peering_cidr_block" {}
+variable "az" {}
+variable "az2" {}
+variable "fms_cidr_block_az2" {}
+
+variable "naming_suffix" {
+  default     = false
+  description = "Naming suffix for tags, value passed from dq-tf-apps"
+}
+
+variable "route_table_id" {
+  default     = false
+  description = "Value obtained from Apps module"
+}
+
+variable "dq_database_cidr_block_secondary" {
+  type = "list"
+}
