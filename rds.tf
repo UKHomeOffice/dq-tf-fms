bdc616f30016a39f70bf14468986c86de0fa72dd
david-wirelab <info@wirelab.co.uk>
2019-05-23 14:44:49 +0100

    add CW alarms

--- rds.tf
+++ rds.tf
@@ -118,6 +118,18 @@ resource "aws_db_instance" "postgres" {
   }
 }
 
+module "rds_alarms" {
+  source = "github.com/UKHomeOffice/dq-tf-cloudwatch-rds"
+
+  naming_suffix                = "${local.naming_suffix}"
+  environment                  = "${var.naming_suffix}"
+  pipeline_name                = "FMS"
+  db_instance_id               = "${aws_db_instance.postgres.id}"
+  free_storage_space_threshold = 13000000000                          # 13GB free space
+  read_latency_threshold       = 0.01                                 # 100 milliseconds
+  write_latency_threshold      = 0.35                                 # 350 milliseconds
+}
+
 resource "aws_ssm_parameter" "rds_fms_username" {
   name        = "rds_fms_username"
   type        = "SecureString"
