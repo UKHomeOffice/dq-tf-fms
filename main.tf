5ea0b0ad16ecd23b2036337838025003a70fa488
David Takacs <info@wirelab.co.uk>
2020-01-14 13:48:52 +0000

    remove redundant Security Group

--- main.tf
+++ main.tf
@@ -17,36 +17,3 @@ resource "aws_route_table_association" "fms_rt_rds_az1" {
   subnet_id      = "${aws_subnet.fms.id}"
   route_table_id = "${var.route_table_id}"
 }
-
-resource "aws_security_group" "fms_sg" {
-  vpc_id = "${var.appsvpc_id}"
-
-  tags {
-    Name = "sg-${local.naming_suffix}"
-  }
-}
-
-resource "aws_security_group_rule" "allow_lambda" {
-  type        = "ingress"
-  description = "Postgres from the Lambda subnet"
-  from_port   = "${var.rds_from_port}"
-  to_port     = "${var.rds_to_port}"
-  protocol    = "${var.rds_protocol}"
-
-  cidr_blocks = [
-    "${var.dq_lambda_subnet_cidr}",
-    "${var.dq_lambda_subnet_cidr_az2}",
-  ]
-
-  security_group_id = "${aws_security_group.fms_sg.id}"
-}
-
-resource "aws_security_group_rule" "allow_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = -1
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = "${aws_security_group.fms_sg.id}"
-}
