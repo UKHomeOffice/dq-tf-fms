5d9ba9a6249bee66b28850d661ea283b6c09cf29
Jwanderema <wanderema@hotmail.co.uk>
2020-05-07 11:00:07 +0100

    add performance insight

--- rds.tf
+++ rds.tf
@@ -112,6 +112,9 @@ resource "aws_db_instance" "postgres" {
   db_subnet_group_name            = "${aws_db_subnet_group.rds.id}"
   vpc_security_group_ids          = ["${aws_security_group.fms_db.id}"]
 
+  performance_insights_enabled          = true
+  performance_insights_retention_period = "7"
+
   lifecycle {
     prevent_destroy = true
   }
