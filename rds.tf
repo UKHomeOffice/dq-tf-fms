e41ce00144e27c6a7f11c103fc7d1e005d77fd12
Srikanth Arunachalam <116078089+s-arunachalam@users.noreply.github.com>
2023-08-31 15:37:10 +0100

    feat/yel8107: Increase allocated space

--- rds.tf
+++ rds.tf
@@ -88,7 +88,7 @@ resource "aws_security_group_rule" "allow_db_out" {
 
 resource "aws_db_instance" "postgres" {
   identifier                      = "fms-postgres-${local.naming_suffix}"
-  allocated_storage               = var.environment == "prod" ? "60" : 70"
+  allocated_storage               = var.environment == "prod" ? "60" : "70"
   storage_type                    = "gp2"
   engine                          = "postgres"
   engine_version                  = var.environment == "prod" ? "14.7" : "14.7"
