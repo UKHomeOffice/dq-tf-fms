005dda9f5d6d23e35ac7d074dfa1313dc33230c3
D-Gurukishore Bolisetty <d-boliseg@U-16XTRFGVJW9XA.dev.dacc.phz>
2026-03-31 16:34:35 +0100

    added snapshot.tf to create snapshot of DB

---
+++ snapshot.tf
@@ -0,0 +1,24 @@
+# =============================================================================
+# ONE-TIME MANUAL SNAPSHOT BEFORE RDS UPGRADE
+# This will create the snapshot ONLY ONCE (on first apply)
+# =============================================================================
+resource "aws_db_snapshot" "fms_manual_snapshot" {
+  db_instance_identifier = aws_db_instance.postgres.identifier   # Use .identifier, not .id
+  db_snapshot_identifier = "fms-pre-upgrade-20260326"
+
+  # Optional but recommended tags
+  tags = {
+    Name        = "fms-pre-upgrade-20260331"
+    Purpose     = "pre-rds-upgrade"
+    Environment = var.environment
+    CreatedBy   = "Terraform"
+  }
+
+  # This ensures it runs after the RDS instance exists
+  depends_on = [aws_db_instance.postgres]
+
+  # This makes sure Terraform never tries to delete or recreate it
+  #lifecycle {
+  #  ignore_changes = [db_snapshot_identifier]
+  #}
+}
